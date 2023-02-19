4d18234bfb7676bf8e63edc33bc1e419b68d46fd
Calin <calinradualexandru@protonmail.com>
2023-02-19 13:32:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+PLACEHOLDER = "[name]"
+
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in names:
+        strip_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, strip_name)
+        with open(f"./Output/ReadyToSend/letter_for_{strip_name}", mode="w") as complete_letter:
+            complete_letter.write(new_letter)
+
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
